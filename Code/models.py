59eb77dc7032349e86ebfd9fc48e0febd1017abe
Emmanuel Wilson <emmanuel.kelly.wilson@gmail.com>
2022-03-24 13:22:40 -0400

    Update models.py

--- Code/models.py
+++ Code/models.py
@@ -180,7 +180,7 @@ class Autoencoder_1(pl.LightningModule):
         )
         self.decoder = nn.Sequential(
                 torch.nn.Linear(latent_dim, condimsval*2*output_channels),
-                Reshape(-1, 2*output_channels, math.sqrt(condimsval), math.sqrt(condimsval)),
+                Reshape(-1, 2*output_channels, np.round(math.sqrt(condimsval)), np.round(math.sqrt(condimsval))),
                 nn.ConvTranspose2d(2*output_channels, 2*output_channels, stride=(1, 1), kernel_size=(3, 3), padding=1),
                 nn.LeakyReLU(act_val),
                 nn.ConvTranspose2d(2*output_channels, 2*output_channels, stride=(2, 2), kernel_size=(3, 3), padding=1),                
