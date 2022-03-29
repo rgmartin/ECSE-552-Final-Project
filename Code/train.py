0b9e24294e95cb349165e7f0d1ad6caf4f89ec14
Emmanuel Wilson <emmanuel.kelly.wilson@gmail.com>
2022-03-28 21:50:36 -0400

    Update train.py

--- Code/train.py
+++ Code/train.py
@@ -86,7 +86,7 @@ def run_ae_train(batch_size=10, max_t=5, data_dir="/content/drive/MyDrive/datate
                    maxpool1=False, 
                    enc_out_dim=2048, 
                    kl_coeff=0.1, 
-                   latent_dim=20)
+                   latent_dim=5)
     model.to(DEVICE)
     if torch.cuda.is_available():
         model.cuda()
